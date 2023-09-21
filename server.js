e05c9d7e1df6dc56b2d4502b42179484a9a7d748
Michael Raillard <mlraillard@yahoo.com>
2023-09-21 16:42:55 -0500

    improve 1

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const http = require('http')
+const express = require('express')
+const cors = require('cors')
 const fs = require('fs')
+
+const app = express()
 const port = 8002
 
-const server = http.createServer(function(req, res) {
+app.get('/onefile', cors(), (req, res) => {
     res.writeHead(200, { 'Content-Type': 'text'})
     fs.readFile('./ckFiles/hoagScriptX.ck', function(error, data) {
         if (error) {
@@ -16,7 +19,7 @@ const server = http.createServer(function(req, res) {
     })
 })
 
-server.listen(port, function(error){
+app.listen(port, function(error){
     if (error) {
         console.log(`error: ${error}`)
     }
